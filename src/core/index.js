fc56d24d0e72129ca39c1be96c17167756d1e946
DESKTOP-UVC4HV8\guant <guantingting@menusifu.cn>
2018-01-04 13:36:01 +0800

    初始

---
+++ src/core/index.js
@@ -0,0 +1,21 @@
+import Vue from './instance/index'
+import { initGlobalAPI } from './global-api/index'
+import { isServerRendering } from 'core/util/env'
+
+initGlobalAPI(Vue)
+
+Object.defineProperty(Vue.prototype, '$isServer', {
+  get: isServerRendering
+})
+
+Object.defineProperty(Vue.prototype, '$ssrContext', {
+  get () {
+    /* istanbul ignore next */
+    console.log(this);
+    return this.$vnode && this.$vnode.ssrContext
+  }
+})
+
+Vue.version = '__VERSION__'
+
+export default Vue
